546745140a6c97c7cdd6acc4d03d8932c5003e20
Nathan <terakilobyte@gmail.com>
2018-03-04 22:32:56 -0500

    cleanup

--- src/server.ts
+++ src/server.ts
@@ -3,79 +3,69 @@ import dotenv from "dotenv"
 import AYLIENTTextAPI from "aylien_textapi"
 import MClient from "./client"
 import { MongoClient, Collection } from "mongodb"
-;(async () => {
-  dotenv.config()
-  const heartbeat = " \n"
+dotenv.config()
+const heartbeat = " \n"
 
-  const options = {
-    hostname: "stream.gitter.im",
-    port: 443,
-    path: "/v1/rooms/" + process.env.GITTER_ROOM_ID + "/chatMessages",
-    method: "GET",
-    headers: { Authorization: "Bearer " + process.env.GITTER_API_TOKEN }
-  }
+const options = {
+  hostname: "stream.gitter.im",
+  port: 443,
+  path: "/v1/rooms/" + process.env.GITTER_ROOM_ID + "/chatMessages",
+  method: "GET",
+  headers: { Authorization: "Bearer " + process.env.GITTER_API_TOKEN }
+}
 
-  const textapi = new AYLIENTTextAPI({
-    application_id: process.env.AYLIEN_APP_ID,
-    application_key: process.env.AYLIEN_APP_KEY
-  })
+const textapi = new AYLIENTTextAPI({
+  application_id: process.env.AYLIEN_APP_ID,
+  application_key: process.env.AYLIEN_APP_KEY
+})
 
-  type FromUser = {
-    username: string
-  }
+type FromUser = {
+  username: string
+}
 
-  type MsgJSON = {
-    text: string
-    fromUser: FromUser
-  }
+type MsgJSON = {
+  text: string
+  fromUser: FromUser
+}
 
-  type DataPoint = {
-    user: string
-    text: string
-  }
+type DataPoint = {
+  user: string
+  text: string
+}
 
-  type Top = {
-    [key: string]: number
-  }
+const blacklistedUsers = ["terakilobyte"]
 
-  const blacklistedUsers = ["terakilobyte", "psyperl"]
-
-  const req = https.request(options, function(res) {
-    res.on("data", function(chunk) {
-      const msg = chunk.toString()
-      if (msg !== heartbeat) {
-        const msgJson: MsgJSON = JSON.parse(msg)
-        const text = msgJson.text.replace(/`{1,3}[\s\S.]*`{1,3}/g, "")
-        if (text.length > 5) {
-          const chatterer: DataPoint = {
-            user: msgJson.fromUser.username,
-            text
-          }
-          if (blacklistedUsers.indexOf(chatterer.user) === -1) {
-            textapi.sentiment(
-              {
-                text: msgJson.text
-              },
-              (err: Error, response: string) => {
-                if (err) {
-                  console.error(err)
-                }
-                MClient.insert(
-                  process.env.MONGODB_URI,
-                  response,
-                  chatterer.user
-                )
+const req = https.request(options, function(res) {
+  res.on("data", function(chunk) {
+    const msg = chunk.toString()
+    if (msg !== heartbeat) {
+      const msgJson: MsgJSON = JSON.parse(msg)
+      const text = msgJson.text.replace(/`{1,3}[\s\S.]*`{1,3}/g, "")
+      if (text.length > 5) {
+        const chatterer: DataPoint = {
+          user: msgJson.fromUser.username,
+          text
+        }
+        if (blacklistedUsers.indexOf(chatterer.user) === -1) {
+          textapi.sentiment(
+            {
+              text: msgJson.text
+            },
+            (err: Error, response: string) => {
+              if (err) {
+                console.error(err)
               }
-            )
-          }
+              MClient.insert(process.env.MONGODB_URI, response, chatterer.user)
+            }
+          )
         }
       }
-    })
+    }
   })
+})
 
-  req.on("error", function(e) {
-    console.log("Something went wrong: " + e.message)
-  })
+req.on("error", function(e) {
+  console.log("Something went wrong: " + e.message)
+})
 
-  req.end()
-})()
+req.end()
