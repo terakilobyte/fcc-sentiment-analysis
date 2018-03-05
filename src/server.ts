0dfe862eb89db9e8c8d30f62e91ad2d56c4283a2
Nathan <terakilobyte@gmail.com>
2018-03-05 07:01:12 -0500

    minor changes

--- src/server.ts
+++ src/server.ts
@@ -39,32 +39,35 @@ const req = https.request(options, function(res) {
   res.on("data", function(chunk) {
     const msg = chunk.toString()
     if (msg !== heartbeat) {
-      const msgJson: MsgJSON = JSON.parse(msg)
-      console.log(msgJson)
-      const text = msgJson.text.replace(/`{1,3}[\s\S.]*`{1,3}/g, "")
-      if (text.length > 5) {
-        const chatterer: DataPoint = {
-          user: msgJson.fromUser.username,
-          text
-        }
-        if (blacklistedUsers.indexOf(chatterer.user) === -1) {
-          textapi.sentiment(
-            {
-              text: msgJson.text
-            },
-            (err: Error, response: string) => {
-              if (err) {
-                console.error(err)
-              } else {
-                MClient.insert(
-                  process.env.MONGODB_URI,
-                  response,
-                  chatterer.user
-                )
+      try {
+        const msgJson: MsgJSON = JSON.parse(msg)
+        const text = msgJson.text.replace(/`{1,3}[\s\S.]*`{1,3}/g, "")
+        if (text.length > 5) {
+          const chatterer: DataPoint = {
+            user: msgJson.fromUser.username,
+            text
+          }
+          if (blacklistedUsers.indexOf(chatterer.user) === -1) {
+            textapi.sentiment(
+              {
+                text: msgJson.text
+              },
+              (err: Error, response: string) => {
+                if (err) {
+                  console.error(err)
+                } else {
+                  MClient.insert(
+                    process.env.MONGODB_URI,
+                    response,
+                    chatterer.user
+                  )
+                }
               }
-            }
-          )
+            )
+          }
         }
+      } catch (e) {
+        console.error(e)
       }
     }
   })
